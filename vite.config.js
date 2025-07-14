54f30b9f004c25f9ea6e453e5a578c2114662b7d
chaubt01 <nvnchau.bt@gmail.com>
2025-07-14 18:20:55 +0700

    Fix: Simplify build configuration for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
-import basicSsl from '@vitejs/plugin-basic-ssl';
-
 export default {
   build: {
     target: 'esnext',
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  },
-  plugins: [basicSsl()],
-  server: {
-    https: true
+    outDir: 'dist'
   }
 }
